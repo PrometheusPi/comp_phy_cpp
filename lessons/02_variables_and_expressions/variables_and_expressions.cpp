4b19101b04077318c9ae80d8d4df6e7c1a81e76b
Richard Pausch <r.pausch@hzdr.de>
2026-01-20 23:33:10 +0100

    add code for variables and expressions

---
+++ lessons/02_variables_and_expressions/variables_and_expressions.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+int main(){
+  // this is a comment
+
+  // datatype: integer
+  int a = 42;
+  int b = -5;
+  int c = a + b;
+  std::cout << "a + b = c" << std::endl;
+  std::cout << a << " + " << b << " = " << c << std::endl;
+
+  std::cout << std::endl;
+
+  // datatype: float / double
+  float x = 3.1415;
+  double y = 1.23e4;
+  double z = x * y;
+  std::cout << "x * y = z" << std::endl;
+  std::cout << x << " * " << y << " = " << z << std::endl;
+
+  std::cout << std::endl;
+
+  // datatype: char / string
+  char letter = 'q';
+  std::string text = "uestion";
+  std::cout << letter << text << std::endl;
+
+  std::cout << std::endl;
+
+  // datatype: vector / array
+  std::vector<double> list1 = {1.2, 3.4, 5.6};
+  std::array<double, 3> list2 = {1.2, 3.4, 5.6};
+  double list3[3] = {1.2, 3.4, 5.6};
+  std::cout << list1[0] << "\t" << list2[2] << "\t" << list3 << std::endl;
+
+  return 0;
+};
