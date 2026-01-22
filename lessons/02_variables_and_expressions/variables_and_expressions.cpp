18137bf14051d314fbaf2b2f79c7b1d1e1e57689
Richard Pausch <r.pausch@hzdr.de>
2026-01-22 22:27:36 +0100

    use both initialization and declaration + assignment

--- lessons/02_variables_and_expressions/variables_and_expressions.cpp
+++ lessons/02_variables_and_expressions/variables_and_expressions.cpp
@@ -8,7 +8,8 @@ int main(){
 
   // datatype: integer
   int a = 42;
-  int b = -5;
+  int b;
+  b = -5;
   int c = a + b;
   std::cout << "a + b = c" << std::endl;
   std::cout << a << " + " << b << " = " << c << std::endl;
@@ -17,7 +18,8 @@ int main(){
 
   // datatype: float / double
   float x = 3.1415;
-  double y = 1.23e4;
+  double y;
+  y = 1.23e4;
   double z = x * y;
   std::cout << "x * y = z" << std::endl;
   std::cout << x << " * " << y << " = " << z << std::endl;
@@ -36,6 +38,8 @@ int main(){
   std::array<double, 3> list2 = {1.2, 3.4, 5.6};
   double list3[3] = {1.2, 3.4, 5.6};
   std::cout << list1[0] << "\t" << list2[2] << "\t" << list3 << std::endl;
-
+  list1[1] = 4.2;
+  std::cout << list1[0] << "\t" << list1[1] << "\t" << list1[2] << std::endl;
+  
   return 0;
 };
