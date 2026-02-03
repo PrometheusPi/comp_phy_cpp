fc9b08921d508ad642916f0e8b19a0d188f62b03
Richard Pausch <r.pausch@hzdr.de>
2026-02-03 22:42:07 +0100

    add more to control

--- lessons/06_control_statements/control.cpp
+++ lessons/06_control_statements/control.cpp
@@ -11,5 +11,57 @@ int main(){
       std::cout << i << " is neither even nor divisible by three" << std::endl;
   }
 
+
+
+  double input = 10.;
+  std::cout << input << " --> ";
+  double result = input > 5 ? 2.0 : 3.0;
+  std::cout << result << std::endl;
+
+
+  std::cout << std::endl;
+  // while loops
+
+  int i = 10;
+  while( i > 0){
+    std::cout << "while: "<< i-- << std::endl;
+  }
+
+
+  std::cout << std::endl;
+  // do while loops
+
+  i = 10;
+  do{
+    std::cout << "do while 1: " << i-- << std::endl;
+  }
+  while (i > 0);
+
+
+  i = -5;
+  do{
+    std::cout << "do while 2: " << i-- << std::endl;
+  }
+  while (i > 0);
+
+
+  std::cout << std::endl;
+  // break and continue
+
+  for(int i = 0; i < 10; i++){
+    if (i > 5){
+      break;
+    }
+    std::cout << "break till 5: " <<i << std::endl;
+  }
+
+  for(int i = 0; i < 10; i++){
+    if (i % 2 == 1){
+      continue;
+    }
+    std::cout << "continue if odd: " << i << std::endl;
+  }
+
+
   return 0;
 }
