e88aa918090bb49d31e3b0b613efdf1909200ee7
Richard Pausch <r.pausch@hzdr.de>
2026-03-22 20:43:09 +0100

    class add setter

--- lessons/07_classes/classes.cpp
+++ lessons/07_classes/classes.cpp
@@ -5,6 +5,7 @@ class BaseParticle{
 public:
   double x;
   double y;
+  double radius;
 
   BaseParticle(std::string priv_name)
   {
@@ -15,6 +16,13 @@ public:
   {
     return this->name;
   }
+
+  void set_radius(double r)
+  {
+    this->radius = r;
+  }
+
+  
 
 private:
     std::string name;
@@ -28,5 +36,8 @@ int main(){
   std::cout << simpleParticle.getName() << "'s x: " << simpleParticle.x << std::endl
 	    << simpleParticle.getName() << "'s y: " << simpleParticle.y << std::endl;
 
+  simpleParticle.set_radius(12.5);
+  std::cout << "radius = " << simpleParticle.radius << std::endl;
+  
   return 0;
 };
